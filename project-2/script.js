f719e4a4849eb2e12189bc79e7e72cdcfb7dacbf
QHarris <harris.quynh@gmail.com>
2023-03-10 11:42:47 -0500

    json demo

--- project-2/script.js
+++ project-2/script.js
@@ -1,166 +1,28 @@
-{
-    title: "hugs and kisses",
-	material: "gold,diamonds",
-	who: "auntlinda",
-    where: "california",
-    type: "bracelet",
-    for: "themself",
-    year: "1990?",
+//create a function to loop through the data and spit out HTML
+const createCollectionHTML = data => {
+    //store the collection grid variable so that we know where to inset the HTML later
+    const collectionGrid = document.getElementById('collection');
+
+//iterate each item of the returned JSON
+    data.forEach(item => {
+        console.log(item);
+        const listItemHTML = `
+            <li>
+                <p class="item-title">${item.name}</p>
+                <p class="item-year">${item.year}</p>
+                <p class="item-description">${item.description}</p>
+                <img src="${item.image}" alt="${item.name}">
+            </li>
+        `
+    });
 }
 
-{
-    title: "charm bracelet",
-    material: "silver",
-    who: "jessica",
-    where: "hongkong",
-    type: "bracelet"
-    for: "me",
-    year: "2018",
-}
-
-{
-    title: "cinnamon necklace",
-    material: "silver",
-    who: "jason",
-    where: "newyork",
-    type: "necklace"
-    for: "me",
-    year: "2022",
-}
-
-{
-    title: "70s belt",
-    material: "silver",
-    who: "quynh",
-    where: "california",
-    type: "belt"
-    for: "themself",
-    year: "2023",
-}
-
-{
-    title: "everyday pearls",
-    material: "pearls",
-    who: "banoi",
-    where: "alabama",
-    type: "necklace",
-    for: "auntlinda",
-    year: "1970?",
-}
-
-{
-    title: "checkered heart",
-    material: "gold",
-    who: "dad",
-    where: "california",
-    type: "necklace",
-    for: "banoi",
-}
-
-{
-    title: "butterfly ring",
-    material: "silver",
-    who: "dad",
-    where: "vietnam",
-    type: "ring",
-    for: "me",
-}
-
-{
-    title: "oil spill ring",
-    material: "gold,",
-    who: "mom",
-    where: "japan",
-    type: "ring",
-    for: "me",
-}
-
-{
-    title: "second family earrings",
-    material: "gold",
-    who: "annikasmom",
-    where: "northcarolina",
-    type: "clip ons",
-    for: "themself",
-}
-
-{
-    title: "pink pearls",
-    material: "pearls",
-    who: "kay",
-    where: "california",
-    type: "necklace",
-    for: "me",
-}
-
-{
-    title: "heart charm",
-    material: "silver",
-    who: "me",
-    where: "california",
-    type: "ring",
-    for: "me",
-}
-
-{
-    title: "bike chain",
-    material: "silver",
-    who: "uncle alan",
-    where: "mexico",
-    type: "necklace",
-    for: "aunt linda",
-}
-
-{
-    title: "parasol",
-    material: "paper, silver",
-    who: "sabina",
-    where: "vietnam",
-    type: "clip ons",
-    for: "me",
-}
-
-{
-    title: "dinos",
-    material: "acrylic, silver",
-    who: "emma",
-    where: "new york",
-    type: "earrings",
-    for: "me",
-}
-
-{
-    title: "everyday pearl",
-    material: "pearl, gold",
-    who: "jason",
-    where: "california",
-    type: "necklace",
-    for: "me",
-}
-
-{
-    title: "pearl ring",
-    material: "pink pearl, rose gold",
-    who: "jason",
-    where: "hawaii",
-    type: "ring",
-    for: "me",
-}
-
-{
-    title: "tardis",
-    material: "silver",
-    who: "aunt linda",
-    where: "california",
-    type: "necklace",
-    for: "me",
-}
-
-{
-    title: "weeping angel",
-    material: "silver",
-    who: "aunt linda",
-    where: "california",
-    type: "necklace",
-    for: "me",
-}
+fetch('collection.json')
+    //first, check if the request is approved/"OK" or if something bad happened
+    //return the document in JSON format
+    .then(response => response.json ())
+    //now that we have the document in JSON format, lets work with it
+    .then(data => {
+        //send it into the function we made aboce (fill in the blank/variable)
+        createCollectionHTML(data);
+    })
