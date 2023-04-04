0897476d6d8932ee4db68a0eee18dfc18d80de09
QHarris <harris.quynh@gmail.com>
2023-04-04 11:19:27 -0400

    gifs

---
+++ class-work/js-demo/jsexample.js
@@ -0,0 +1,22 @@
+// add id to json
+
+//"class": "color-red"
+
+
+
+<li id="${item.id}" class="list-item ${item.class}"></li>
+<img src="${item.posterImage}" alt=
+
+// CSS
+
+// .color-blue {
+//     color: Blue;
+
+// }
+
+// .color-red {
+//     color: red;
+
+// }
+
+collectionList.insertAdjacentHTML ('beforeend', listItem)
